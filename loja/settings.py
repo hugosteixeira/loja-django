70acbf44bb4510584db477b1c6c5fb5c71e33356
Hugo Teixeira <hugosteixeira@hotmail.com>
2019-04-03 19:44:25 -0300

    don't know

--- loja/settings.py
+++ loja/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -118,3 +118,4 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'produto/static/produtos')
+django_heroku.settings(locals())
